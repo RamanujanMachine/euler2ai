74023bacc4e47d6176f417357eb91a8c74c6af72
Tomer Raz <you@example.com>
2024-12-09 01:08:16 +0200

    Add gather iterator and test
    Can now easily iterate over a gather as collected in arxiv_dataset_gather.py to apply more advanced stages of the pipeline. The test comes along with a test directory that is a mock result of arxiv_dataset_gather.py

---
+++ arxiv_dataset_iterator_test.py
@@ -0,0 +1,38 @@
+from arxiv_dataset_iterator import gather_iterator
+
+
+def test_gather_iterator():
+    ids_of_interest = ['id0', 'id1', 'id2', 'id3', 'id4', 'id5', 'id6', 'id7', 'id8', 'id9', 'id10']
+    dir_origin = 'arXiv_dataset_iterator_test_files'
+    dir_destin = 'arXiv_destin'
+
+    kwargs = {'start_index': 3,
+              'end_index': 8,
+              'use_dirs': True,
+              'make_dirs': False, # do not change this, will check for and create dirs
+              'break_bool': True, # should not even be checked
+              'exist_ok': False, # to be safe - should not even be checked
+              'verbose': False,
+              'print_every': None}
+
+    ids_passed = []
+    for i, args_dict in enumerate(gather_iterator(ids_of_interest, dir_origin, dir_destin, **kwargs), start=kwargs['start_index']):
+        assert args_dict['ind'] == i # index in ids of interest
+        assert args_dict['id'] == f'id{i}'
+        ids_passed.append(args_dict['id'])
+        assert args_dict['file_origin'] == f"{dir_origin}/0-10__id0__to__id10/{i}__id{i}.json"
+        assert args_dict['file_destin'] == f"{dir_destin}/0-10__id0__to__id10/{i}__id{i}.json"
+        assert args_dict['gather'] == {f'id{i}': {'file1': [{'e': 'equation', 'l': 1, 't': 'b'}]}}
+        print(i)
+
+    assert ids_passed == ['id3', 'id4', 'id5', 'id6', 'id7', 'id8']
+
+
+
+# import os
+# def create_test_directory():
+#     os.makedirs("arXiv_dataset_iterator_test_files/0-99__id0__to__id99", exist_ok=True)
+#     for i in range(0, 100):
+#         with open(f"arXiv_dataset_iterator_test_files/0-99__id0__to__id99/{i}__id{i}.json", "w") as f:
+#             gather = {f'id{i}': {'file1': [{'e': 'equation', 'l': 1, 't': 'b'}]}}
+#             json.dump(gather, f)
