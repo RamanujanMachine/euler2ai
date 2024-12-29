044fba43fe7fc149f525131bdf5b090596571f3f
Tomer Raz <you@example.com>
2024-12-29 15:39:28 +0200

    Bugfix
    There may be multiple symbolic limit candidatesthat have the same value when evaluated. These cases were previously treated as conflicting and therefore the limit was designated None.

--- arxiv_dataset_13_merge_sources.py
+++ arxiv_dataset_13_merge_sources.py
@@ -6,12 +6,16 @@ import sympy as sp
 # input
 DATAFRAMES_TO_MERGE = [
     r"C:\Users\totos\Desktop\11 - arXiv_pcfs_dataframe_identified_limits_merged_cfs_and_recurrences\pcfs_0-154047.pkl",
+    r"C:\Users\totos\Desktop\11 - arXiv_pcfs_dataframe_identified_limits_merged_cfs_and_recurrences\pcfs_154048-199999",
+    r"C:\Users\totos\Desktop\11 - arXiv_pcfs_dataframe_identified_limits_merged_cfs_and_recurrences\pcfs_200000-349999",
+    r"C:\Users\totos\Desktop\11 - arXiv_pcfs_dataframe_identified_limits_merged_cfs_and_recurrences\pcfs_421641-435736",
+    r"C:\Users\totos\Desktop\11 - arXiv_pcfs_dataframe_identified_limits_merged_cfs_and_recurrences\pcfs_math.CA_0-22736",
     r"C:\Users\totos\Desktop\12 - manual_pcfs\manual_pcfs_from_29.10.24.pkl",
     r"C:\Users\totos\Desktop\12 - cmf_pcfs\cmf_pcfs-('pi_cmf', 'symmetric_pi_cmf', '2F1')-max_i=8-generate_folds=True.pkl",
 ]
 
 # output
-SAVE_FILE = r'pcfs_28.12.24.pkl'
+SAVE_FILE = r'pcfs_28.12.24_test.pkl'
 
 # defaults
 SAVE_DIR = r"C:\Users\totos\Desktop\13 - merge_dataframes_and_sources"
@@ -22,6 +26,13 @@ SAVE_PATH = SAVE_DIR + '\\' + SAVE_FILE
 
 def normalize_a_b(sympy_exp):
     return str(sp.expand(sympy_exp))
+
+
+def try_float(x):
+    try:
+        return float(x)
+    except:
+        return None
 
 
 def merge_sources(df):
@@ -40,6 +51,14 @@ def merge_sources(df):
 
         # Collect unique limits, excluding None
         unique_limits = list({limit for limit in group['limit'] if limit is not None})
+        # apparently this does not fully work, need to compare floats because there are still different representations
+        # of the same float
+
+        lim2floats = {lim: try_float(lim.evalf(10)) for lim in unique_limits}
+        floats2lim = {v: k for k, v in lim2floats.items() if v is not None}
+        unique_floats = list(set([flt for flt in lim2floats.values() if flt is not None]))
+        num_floats = len(unique_floats)
+        limit = floats2lim[unique_floats[0]] if num_floats == 1 else None
 
         # Gather sources as dictionaries
         source_types = group['source_type'].unique()
@@ -49,7 +68,8 @@ def merge_sources(df):
         results.append({
             'a': a,
             'b': b,
-            'limit': None if (len(unique_limits) == 0 or len(unique_limits) > 1) else unique_limits[0],
+            'limit': limit,
+            'first20convergents' : group['first20convergents'],
             'source_types': source_types,
             'limit_candidates': unique_limits,
             'sources': sources
@@ -105,14 +125,15 @@ def add_row_to_processed(processed_df, new_row):
             'a': a,
             'b': b,
             'limit': new_row['limit'],
+            'first20convergents' : new_row['first20convergents'],
             'source_types': [new_row['source_type']],
             'limit_candidates': [new_row['limit']] if new_row['limit'] is not None else [],
             'sources': [{
-                'limit': new_row['limit'],
                 'origin_formula_type': new_row['origin_formula_type'],
                 'source_type': new_row['source_type'],
                 'source': new_row['source'],
-                'metadata': new_row['metadata']
+                'metadata': new_row['metadata'],
+                'limit': new_row['limit']
             }]
         }
         processed_df = pd.concat([processed_df, pd.DataFrame([new_entry])], ignore_index=True)
