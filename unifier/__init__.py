4b902cbaed0e80c918d1eab64acc2be1288b53c1
Tomer Raz <you@example.com>
2025-03-19 01:07:47 +0200

    Update unifier.__init__

--- unifier/__init__.py
+++ unifier/__init__.py
@@ -1,6 +1,6 @@
 from .pcf import PCF
 from .coboundary_solver import CobViaLim, PCFCobViaLim
-from .identify import identify
+from .identify import identify, identification_loop, identify_pcf_limit
 from .find_initial import find_initial
 from .coboundary_graph import recursive_coboundary_graph
 from .recurrence_transforms import (
@@ -11,13 +11,15 @@ from .recurrence_transforms import (
     CobTransformAsPCF,
     CobTransformInflate,
     CobTransformShift
-    )
+)
 
 __all__ = [
     'PCF',
     'CobViaLim',
     'PCFCobViaLim',
     'identify',
+    'identification_loop',
+    'identify_pcf_limit',
     'find_initial',
     'recursive_coboundary_graph',
     'RecurrenceTransform',
